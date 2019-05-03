58183bd66c0b37c48d06dd82ca9918ff018884fa
LuizaAS <luizajosouza@gmail.com>
2019-05-02 23:57:16 -0300

    funciona

--- starwars.js
+++ starwars.js
@@ -5,19 +5,30 @@
 //  - Quando um filme for clicado, exibir sua introdução
 
 
+
 fetch('https://swapi.co/api/films')
-  .then(r => r.json())
-  .then( (resp) => {
+  .then(request => request.json())
+  .then((response) => {
     let moviesContent = document.querySelector('#movies');
     let list = document.createElement("UL");
-    resp.results.forEach((movie) => {
+    response.results.forEach((movie) => {
       let item = document.createElement("LI");
-      item.innerHTML = movie.title
+      item.innerHTML = movie.title;
       list.appendChild(item)
       item.addEventListener('click', (e) => {
+        localStorage.setItem("last", JSON.stringify(movie));
         let text = document.querySelector('.reading-animation');
-        text.innerHTML = movie.opening_crawl
-      })
+        text.innerHTML = movie.opening_crawl;
+      });
     })
-    moviesContent.appendChild(list)
+    moviesContent.appendChild(list);
+    return response.results;
   })
+  .then((response) => {
+    let lastMovie = JSON.parse(localStorage.getItem("last"));
+    if (lastMovie == null) lastMovie = response[0];
+    let text = document.querySelector('.reading-animation');
+    text.innerHTML = lastMovie.opening_crawl;
+  });
+
+
